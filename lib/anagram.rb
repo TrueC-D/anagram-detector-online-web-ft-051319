28d5dc58dc74fdf0c624e873de2b9677cc7f7e96
Alethea Cary <aletheacary@gmail.com>
2019-05-26 13:30:23 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initilialize(word)
+    @word = word
+  end
+  
+  def match
+    @word.split("").sort.match
+    
+  end
+  
+  
+end
